7c7c4a6e3175da61d77634e73e51b79f97cd80a4
Jonas Svensson <jonas.m.svensson@gmail.com>
2016-08-27 14:11:48 +0200

    Added fireworks

---
+++ Assets/Scripts/ParticleSystemHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemHandler : MonoBehaviour {
+	public ParticleSystem m_System;
+
+	// Use this for initialization
+	void Start () {
+		m_System.Emit (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {	       
+	}
+}

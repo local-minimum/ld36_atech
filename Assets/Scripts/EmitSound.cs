95309c2c022da1bebcc28b724a369e04d8cc86bc
Jonas Svensson <jonas.m.svensson@gmail.com>
2016-08-27 18:15:05 +0200

    more work

---
+++ Assets/Scripts/EmitSound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmitSound : MonoBehaviour {
+
+	public AudioSource source;
+
+	void OnParticleCollision(GameObject other) {
+		source.Play ();
+	}
+
+}

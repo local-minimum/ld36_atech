7d1565fbcb6d00bc0f8a83f512ababb37c4ed259
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-08-27 10:52:50 +0200

    Bad paralaxing started

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paralax : MonoBehaviour {
+
+	[SerializeField] Camera cam;
+	[SerializeField] float factor = 1;
+	Vector3 origin;
+	float depth;
+
+	void Start () {
+		origin = transform.position;
+		depth = transform.position.z;
+	}
+	
+	Vector2 angles {
+		get {
+			Vector3 delta = origin - cam.transform.position;
+			delta /= delta.magnitude;
+			return new Vector2 (Mathf.Acos(delta.x), Mathf.Acos(delta.y));
+		}
+	}
+
+	void Update () {
+	
+	}
+}

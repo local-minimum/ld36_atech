88020db4ef956e34c3854fa9abb8933acdb452c4
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-08-27 14:58:27 +0200

    Starting script interactions in workshop

---
+++ Assets/Scripts/Workshop/RocketSlot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+
+public class RocketSlot : MonoBehaviour {
+
+    [SerializeField]
+    ItemType itemType;
+
+    StoreItem item;
+
+    Workshop workshop;
+
+    public StoreItem Item
+    {
+        get
+        {
+            return item;
+        }
+
+        set
+        {
+            item = value;
+        }
+    }
+
+    void Awake()
+    {
+        workshop = FindObjectOfType<Workshop>();
+    }
+
+    void OnMouseEnter()
+    {
+        if (item == null)
+        {
+            workshop.Emit(this, SlotEvent.Hover);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (item == null)
+        {
+            workshop.Emit(this, SlotEvent.Exit);
+        }
+    }
+}
---
+++ Assets/Scripts/Workshop/StoreItem.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreItem : MonoBehaviour {
+
+    public ItemType itemType = ItemType.Charges;
+
+    Workshop workshop;
+    RocketSlot slot;
+    StoreItemEvents state;
+
+    void Awake()
+    {
+        workshop = FindObjectOfType<Workshop>();
+    }
+
+    void LateUpdate () {
+	
+	}
+
+    void OnEnable()
+    {
+        workshop.OnRocketSlotAction += Workshop_OnRocketSlotAction;
+    }
+
+    void OnDisalbe()
+    {
+        workshop.OnRocketSlotAction -= Workshop_OnRocketSlotAction;
+    }
+
+    private void Workshop_OnRocketSlotAction(RocketSlot slot, SlotEvent type)
+    {
+        if (state == StoreItemEvents.Drag) {
+            if (type == SlotEvent.Hover)
+            {
+                this.slot = slot;
+            } else if (type == SlotEvent.Exit)
+            {
+                this.slot = null;
+            }
+         }
+    }
+
+    public void Hover()
+    {
+        workshop.Emit(this, StoreItemEvents.Hover);        
+    }
+
+    public void Drag()
+    {
+        workshop.Emit(this, StoreItemEvents.Drag);
+    }
+
+    public void Release()
+    {
+        if (slot == null)
+        {
+            workshop.Emit(this, StoreItemEvents.Return);
+        } else
+        {
+            slot.Item = this;
+            workshop.Emit(this, StoreItemEvents.Slotted);
+
+            slot = null;
+        }
+
+        
+    }
+}

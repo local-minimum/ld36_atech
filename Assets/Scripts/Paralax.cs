406134c282ee568beec4aef62cee849d9fdf45e5
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-08-27 22:52:24 +0200

    Towards working parts

--- Assets/Scripts/Paralax.cs
+++ Assets/Scripts/Paralax.cs
@@ -4,24 +4,25 @@ using System.Collections;
 public class Paralax : MonoBehaviour {
 
 	[SerializeField] Camera cam;
-	[SerializeField] float factor = 1;
+	[SerializeField] float factor = 30;
 	Vector3 origin;
 	float depth;
 
 	void Start () {
 		origin = transform.position;
-		depth = transform.position.z;
+		depth = transform.position.z - cam.transform.position.z;
 	}
 	
 	Vector2 angles {
 		get {
-			Vector3 delta = origin - cam.transform.position;
-			delta /= delta.magnitude;
-			return new Vector2 (Mathf.Acos(delta.x), Mathf.Acos(delta.y));
+			Vector3 delta = origin - cam.transform.position;			
+			return new Vector2 (Mathf.Atan2(delta.x, delta.z), Mathf.Atan2(delta.y, delta.z));
 		}
 	}
 
 	void Update () {
-	
+        Vector2 a = angles;
+        
+        transform.position = new Vector3(Mathf.Tan(a.x) / depth * factor, Mathf.Tan(a.y) / depth * factor, origin.z); 
 	}
 }

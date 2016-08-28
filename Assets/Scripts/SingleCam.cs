e73e8f4358093ac9cbad9843152a065d53ec3929
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-08-28 15:56:00 +0200

    Single cam

---
+++ Assets/Scripts/SingleCam.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleCam : MonoBehaviour {
+
+    static SingleCam _cam;
+
+	void Awake () {
+	    if (_cam != null && _cam != this)
+        {
+            Destroy(this);
+        }
+        _cam = this;
+        DontDestroyOnLoad(this);
+	}
+}

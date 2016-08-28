093bcbdcbaf3abf43e054de5b4fdc5e3ae1646c1
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-08-28 22:09:59 +0200

    Help text in place

---
+++ Assets/Scripts/Workshop/HoverHelp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverHelp : MonoBehaviour {
+
+    [SerializeField]
+    string helpTitle;
+
+    [SerializeField]
+    string helpText;
+
+    [SerializeField]
+    float hoverTimeout = 3f;
+
+    [SerializeField]
+    Text Title;
+
+    [SerializeField]
+    Text Body;
+
+    void Start()
+    {
+        Title.text = helpTitle;
+        Body.text = helpText;
+    }
+}
